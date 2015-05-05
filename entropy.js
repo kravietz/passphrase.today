715d8b45963a8d47a63f358376b74924a2431636
Pawel Krawczyk <p.krawczyk@kainos.com>
2015-05-05 12:37:45 +0100

    implement BlockChain hash rate lookup

--- entropy.js
+++ entropy.js
@@ -4,6 +4,15 @@
  */
 "use strict";
 
+/** @const */ var /** string */ BLOCKCHAIN_STATS_URL = 'https://blockchain.info/stats?format=json';
+/** @const */ var /** string */ LS_HASH_RATE = 'blockchain_hash_rate';
+/** @const */ var /** string */ LS_HASH_RATE_TIMESTAMP = 'blockchain_hash_rate_timestamp';
+/**
+ * If on-line hash rate is unreachable we use default value which was recorded on
+ * 5 May 2015
+ */
+/** @const */ var /** float */ DEFAULT_HASH_RATE = 3.457814595843686E8;
+
 /**
  * Estimates entropy of passphrase given the dictionary used to generate it.
  * @constructor
@@ -15,9 +24,66 @@ function EntropyEstimator(/** Object */ dictionary) {
 
 EntropyEstimator.prototype.nistMap = [4, 2, 2, 2, 2, 2, 2, 2, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5];
 
-EntropyEstimator.prototype.getMinEntropy = function(pass) {
+EntropyEstimator.prototype.getMinEntropy = function (pass) {
     // run all entropy estimators and
     return Math.min(this.getNist(pass), this.getShannon(pass), this.getWord(pass));
+};
+
+/**
+ * Returns BlockChain hash rate in Gh/s. If network is not available or a cached value
+ * is fresh, it will return a default or cached value.
+ * @returns {number}
+ */
+EntropyEstimator.prototype.getHashRate = function () {
+    var hash_rate = DEFAULT_HASH_RATE;
+    var blockchain_hash_rate = 0;
+    var hash_rate_timestamp = new Date();
+
+    // try to read cached value
+    if ('localStorage' in window) {
+        hash_rate_timestamp = new Date(localStorage.getItem(LS_HASH_RATE_TIMESTAMP)) || new Date();
+        hash_rate = parseFloat(localStorage.getItem(LS_HASH_RATE)) || DEFAULT_HASH_RATE;
+        // if cached hash rate younger than 7 days, use the cached value
+        if (new Date() - hash_rate_timestamp < 60*60*24*7*1e3) {
+            return hash_rate;
+        }
+    }
+
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        var DONE = this.DONE || 4;
+        if (this.readyState === DONE) {
+            try {
+                var stats = JSON.parse(req.responseText);
+                blockchain_hash_rate = stats['hash_rate'];
+            } catch (e) {
+                console.warn('Cannot parse BlockChain response:', e);
+                return hash_rate; // return default
+            }
+
+            // only record the maximum value seen from BlockChain
+            // the current hash rate fluctuates, but for our purposes
+            // we're interested in the highest one
+            if (blockchain_hash_rate > hash_rate) {
+                hash_rate = blockchain_hash_rate;
+                if ('localStorage' in window) {
+                    localStorage.setItem(LS_HASH_RATE, hash_rate);
+                    localStorage.setItem(LS_HASH_RATE_TIMESTAMP, new Date());
+                }
+            }
+
+            // this is the "fully successful" execution path
+            console.log('BlockChain.info hash rate', stats['hash_rate]']);
+            return hash_rate;
+        }
+    };
+    try {
+        req.open('GET', BLOCKCHAIN_STATS_URL, true);
+        req.send(null);
+    } catch (e) {
+        console.warn('Cannot connect to BlockChain:', e);
+        return hash_rate; // return default
+    }
 };
 
 EntropyEstimator.prototype.getShannon = function (pass) {
@@ -30,8 +96,8 @@ EntropyEstimator.prototype.getShannon = function (pass) {
 EntropyEstimator.prototype.getShannonExplain = function (pass) {
     var passText = pass.toString();
     return ['Because the passphrase has', passText.length, ' characters',
-            'and each character in this dictionary provides on average ',
-            dictionary.max_entropy_per_char.toFixed(2),' bits of entropy'].join(' ');
+        'and each character in this dictionary provides on average ',
+        dictionary.max_entropy_per_char.toFixed(2), ' bits of entropy'].join(' ');
 };
 
 EntropyEstimator.prototype.getNist = function (pass) {
@@ -60,7 +126,7 @@ EntropyEstimator.prototype.getNist = function (pass) {
 EntropyEstimator.prototype.getNistExplain = function (pass) {
     var passText = pass.toString();
     return ['Because the passphrase has', passText.length, ' characters',
-            'and the result is a sum of NIST entropy values on their positions'].join(' ');
+        'and the result is a sum of NIST entropy values on their positions'].join(' ');
 };
 
 EntropyEstimator.prototype.getWord = function (pass) {
@@ -68,6 +134,6 @@ EntropyEstimator.prototype.getWord = function (pass) {
 };
 EntropyEstimator.prototype.getWordExplain = function (pass) {
     return ['Because the passphrase has', pass.length, ' words',
-            'and each word in this dictionary on average provides',
-            this.dictionary.entropy_per_word.toFixed(2),' bits of entropy'].join(' ');
+        'and each word in this dictionary on average provides',
+        this.dictionary.entropy_per_word.toFixed(2), ' bits of entropy'].join(' ');
 };
